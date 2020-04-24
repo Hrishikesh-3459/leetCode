956567fb52cb427aecd711fbd87f21cb970bd28b
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-04-24 15:23:06 +0530

    Tried solving question 20

--- prob_20.py
+++ prob_20.py
@@ -1,18 +1,23 @@
 def isValid(s):
-    sq = 0
-    curl = 0
-    cir = 0
-    for i in s:
-        if(i == '[' or i == ']'):
-            sq += 1
-        elif(i == '{' or i == '}'):
-            curl += 1
-        elif(i == '(' or i == ')'):
-            cir += 1
-    if((sq%2 == 0) and (curl%2 == 0) and (cir%2 == 0)):
+    brack = []
+    opposite = { ')': '(', '}': '{', ']': '['}  
+    ope_n = ['[', '(', '{']
+    if(s[0] == '}' or s[0] == ')' or s[0] == ']'):
+        return(False)
+    for i in range(len(s)):
+        if(s[i] in ope_n):
+            brack.append(s[i])
+        elif(brack[-1] == opposite[s[i]]):
+            brack.remove(brack[-1])
+        else:
+            return(False)
+    if(len(brack) == 0):
         return(True)
     else:
         return(False)
-print(isValid("(]"))
+        
+        
+
+print(isValid("[])"))
 
     
