1cdc7806279871c06b3ba95144915cfd5cdcb004
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-04-28 17:34:32 +0530

    Added solution to problem 66

---
+++ prob_66.py
@@ -0,0 +1,12 @@
+def plusOne(digits):
+    num = ""
+    for i in digits:
+        num += str(i)
+    out = int(num) + 1
+    ret = []
+    for i in str(out):
+        ret.append(i)
+    return(ret)
+print(plusOne([9]))
+
+    
