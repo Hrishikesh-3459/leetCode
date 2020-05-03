e6886b32e852dfc72b86c7610a412ecf77cd4472
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-05-03 12:23:00 +0530

    Added solution to problem 26

---
+++ prob_26.py
@@ -0,0 +1,13 @@
+def removeDuplicates(nums):
+     def removeDuplicates(self, nums):
+        if(len(nums) == 0):
+            return 0
+        for i in range(len(nums)-1, 0, -1):
+            if (nums[i] == nums[i-1]):
+                nums.pop(nums.index(nums[i-1]))
+
+        # print(nums)
+        return(len(nums))
+        
+
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
