fe98104695b76522e474103f610921ea8868eb69
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-05-11 16:33:48 +0530

    Add Digits

---
+++ prob_258.py
@@ -0,0 +1,7 @@
+def addDigits(self, num: int) -> int:
+        while (len(str(num)) != 1):
+            su = 0
+            for i in str(num):
+                su += int(i)
+            num = su
+        return (num)
