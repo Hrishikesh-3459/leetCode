6d762daafca2c81f2f288b010ef511d6f7b58070
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-06-12 14:30:20 +0530

    Merge Two Sorted Lists

--- prob_21.py
+++ prob_21.py
@@ -1,65 +1,32 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-        
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1, l2):
-        if (l1 == None and l2 == None):
-            return
-        elif (l1 == None):
-            return(l2)
-        elif (l2 == None):
-            return(l1)
-        currentNode1 = l1
-        currentNode2 = l2
-        linkedlist = LinkedList()
-        while True:
-            if (currentNode1.val < currentNode2.val):
-                node = ListNode(currentNode1.val)
-                linkedlist.insert(node)
-                if (currentNode1.next == None):
-                    linkedlist.insert(currentNode2)
-                    break
-                else:
-                    currentNode1 = currentNode1.next
-            elif (currentNode1.val > currentNode2.val):
-                node = ListNode(currentNode2.val)
-                linkedlist.insert(node)
-                if (currentNode2.next == None):
-                    linkedlist.insert(currentNode1)
-                    break
-                else:
-                    currentNode2 = currentNode2.next
-            else:
-                node = ListNode(currentNode1.val)
-                linkedlist.insert(node)
-                if (currentNode1.next == None):
-                    linkedlist.insert(currentNode2)
-                    break
-                else:
-                    currentNode1 = currentNode1.next
-                node = ListNode(currentNode2.val)
-                linkedlist.insert(node)
-                if (currentNode2.next == None):
-                    linkedlist.insert(currentNode1)
-                    break
-                else:
-                    currentNode2 = currentNode2.next
-        return(linkedlist.head)
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def insert(self, newNode):
-        if self.head is None:
-            self.head = newNode
-        else:
-            lastNode = self.head 
-            while True:
-                if lastNode.next is None:
-                    break
-                lastNode = lastNode.next
-            lastNode.next = newNode
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        cur = dummy
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        while l1 and l2:
+            if l1.val <= l2.val:
+                dummy.next = l1
+                l1 = l1.next
+            elif l1.val > l2.val:
+                dummy.next = l2
+                l2 = l2.next
+            dummy = dummy.next
+        if l1 != None:
+            while l1:
+                dummy.next = l1
+                l1 = l1.next
+                dummy = dummy.next
+        if l2 != None:
+            while l2:
+                dummy.next = l2
+                l2 = l2.next
+                dummy = dummy.next
+        return cur.next
