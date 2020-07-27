40f0fde325aa4b5cc9d18c0980e562a40dad6548
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2020-07-27 15:46:14 +0530

    Number of Good Pairs

--- prob_1512.py
+++ prob_1512.py
@@ -1,8 +1,5 @@
-def canMakeArithmeticProgression(arr):
-    arr.sort()
-    val = abs(arr[1] - arr[0])
-    for i in range(2, len(arr)):
-        cur = abs(arr[i] - arr[i-1])
-        if cur != val:
-            return False
-    return True
+def numIdenticalPairs(nums):
+    ans = 0
+    for n,i in enumerate(nums):
+        ans += nums[n+1:].count(i)
+    return ans
